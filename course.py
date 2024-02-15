b2128ed3a61ad484519a72186d4eeec7e7fab5ed
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-02-15 10:26:09 +0100

    Created class definition

---
+++ course.py
@@ -0,0 +1,6 @@
+class Course:
+    def __init__(self, name, class_name, subject, teacher = None):
+        self.name = name
+        self.class_name = class_name
+        self.subject = subject
+        self.teacher = teacher
