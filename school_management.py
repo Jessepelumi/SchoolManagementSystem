ee479bf4b7bb72942593791ef04ce1ee2595691b
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-02-17 07:09:56 +0100

    added top level school management file

---
+++ school_management.py
@@ -0,0 +1,15 @@
+print("Welcome to xoxo school manager")
+print("Enter the appropriate command to perform the corresponding operation")
+print("1 - Login as administrator\n2 - Log in as teacher\nq - Quit")
+
+# while True:
+#     command = input("Enter your preferred command or 'q' to quit: ")
+#     if command == "1":
+#         # code for administrator login
+#     elif command == "2":
+#         # code for teacher login
+#     elif command.lower() == 'q':
+#         print("Exiting...")
+#         break
+#     else:
+#         print("Invalid command. Please enter 1, 2 or 'q to quit'.")
